5e15bb5d99d42b1bb6f6ff5bb03cd85480c72019
Tudor <tudor204@hotmail.com>
2025-05-15 13:36:43 +0200

    funcion mover clase pelota

--- figura_class.py
+++ figura_class.py
@@ -1,7 +1,7 @@
 import pygame as pg
 
-class raqueta:
-    def __init__(self,pos_x,pos_y,w=20,h=50,color=(255,255,255),vx=1,vy=1):
+class Raqueta:
+    def __init__(self,pos_x,pos_y,w=20,h=120,color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.w = w
@@ -11,10 +11,20 @@ class raqueta:
         self.vy = vy
     
     def dibujar(self,pantalla):
-        pg.draw.rect(pantalla,self.color,(self.pos_x,self.pos_y,self.w,self.h))
+        pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))
+
+    def mover(self,tecla_arriba, tecla_abajo,y_max=600,y_min=0):
+        
+        estado_teclado = pg.key.get_pressed()
+    
+        if estado_teclado[tecla_arriba] == True and self.pos_y > y_min +(self.h/2):
+            self.pos_y -=3
+
+        if estado_teclado[tecla_abajo] == True and self.pos_y < y_max -(self.h/2):
+            self.pos_y +=3
 
 
-class pelota:
+class Pelota:
     def __init__(self,pos_x,pos_y,color=(255,255,255),radio=20,vx=1,vy=1):
         self.pos_x = pos_x
         self.pos_y = pos_y
@@ -22,9 +32,25 @@ class pelota:
         self.radio = radio
         self.vx = vx 
         self.vy = vy
+        self.contadorDerecho=0
+        self.contadorIzquierdo=0
     
     def dibujar(self,pantalla):
         pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
 
+    def mover(self,x_max=800, y_max=600):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        if self.pos_y >= y_max-self.radio or self.pos_y < 0+self.radio:
+            self.vy *= -1
+        
+        if self.pos_x >= x_max+self.radio*5: #limite derecho
+            self.vx *= -1
+            self.contadorIzquierdo +=1
+
+        if self.pos_x < 0-self.radio*5:#limite izquierdo
+            self.vx *= -1
+            self.contadorDerecho +=1
+        
 
     
--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import pygame as pg 
-from figura_class import pelota,raqueta
+from figura_class import Pelota,Raqueta
 
 pg.init()
 
 pantalla_principal = pg.display.set_mode((800,600))
 pg.display.set_caption("Pong")
-
-pelota = pelota(400,300)
-raqueta1 = raqueta(0+20,300-(50/2))
-raqueta2 = raqueta(800-30,300-(50/2))
+#definir tasa de refresco de nuestro bucle de fotogramas, fps=fotograma por segundo
+tasa_refresco = pg.time.Clock()
+pelota = Pelota(400,300)
+raqueta1 = Raqueta(10,300)
+raqueta2 = Raqueta(790,300)
 
 
 game_over = False
 
 while not game_over:
+    #obener la tasa de refresco en milisegundos
+    valor_tasa = tasa_refresco.tick(300)#variables para controlar la velocidad entre fotogramas
+    #print(valor_tasa)
     
     for eventos in pg.event.get():
         if eventos.type == pg.QUIT:
             game_over = True
+
+
+    raqueta1.mover(pg.K_w,pg.K_s)
+    raqueta2.mover(pg.K_UP,pg.K_DOWN)
+    pelota.mover()
+    print("punto Derecho", pelota.contadorDerecho)
+    print("punto Izquierdo",pelota.contadorIzquierdo)
+    
 
     pantalla_principal.fill( (0,128,94 ) )
     pg.draw.line(pantalla_principal,(255,255,255),(400,0),(400,600),10)
